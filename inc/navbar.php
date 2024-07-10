9a0992083cb249c322c442f1797646b7287ae5ee
AHMED HASSAN <151830489+AHMED1CB@users.noreply.github.com>
2024-07-10 19:55:36 +0300

    Add files via upload

---
+++ inc/navbar.php
@@ -0,0 +1,39 @@
+    <link rel="stylesheet" href="../style/css/nav.css">
+<div id="navbar">
+
+            <div class="logo">
+                <div class="logo_img">
+                    <a href="index.php"></a>
+                </div>
+                <h1>
+                <a href="index.php">
+                    الجيل البرمجي العربي
+                </a>
+                </h1>
+               
+            </div>
+<div id="toggle_btn" >
+        <span></span>
+        <span></span>
+        <span></span>
+    </div>
+    <div id="nav">
+  
+    <ul>
+
+    <li> <a href="home.php" id="home" class="active">الرئيسية</a></li>
+        <li> <a href="roadmap.php" id="roadmap">المسارات التعليمية</a></li>
+        <li> <a href="lang_programing.php" id="lang_programing">لغات البرمجة</a></li>
+        <li> <a href="profile.php" id="profile">الملف الشخصي</a></li>
+        <li> <a href="communitie.php" id="communitie">المجتمع</a></li>
+        <li> <a href="blogger/blogger.php" id="blogger">المدونة</a></li>
+       
+    </ul>
+    </div>
+</div>
+<script src="js/add_remove_active.js">
+</script>
+<script src="js/toggle_btn_nav.js">
+</script>
+
+
